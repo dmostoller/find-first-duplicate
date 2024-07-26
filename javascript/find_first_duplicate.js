e55b559f8126fb3766cd3a81639237a96bf0f6a5
David Mostoller <dmostoller@gmail.com>
2024-07-26 00:04:16 -0400

    completed

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,15 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let newArr = new Set();
+
+  for (let num of arr ) {
+    if (newArr.has(num)) {
+        return num;
+    }
+    newArr.add(num);
+
+  }
+  return -1;
 }
 
 if (require.main === module) {
@@ -11,9 +21,21 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log("");
+
+  console.log("Expecting: 20");
+  console.log("=>", findFirstDuplicate([10, 20, 30, 40, 20, 40]));
 }
 
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+// initialize an empty array newArray
+// interate through each element in the input array
+// check if that element exists in the newArray
+// if not, append it to the array
+// if it does, return it
+// if nothing gets returned by the end of the array return -1
+
 // And a written explanation of your solution
